b069fac8e3dd64f921295d123b986a0cf3068621
Nikhil Yeole <nikhil.yeole36@gmail.com>
2021-06-02 17:56:15 +0530

    Fixed current date bug.

--- src/component/api/TableAPI.jsx
+++ src/component/api/TableAPI.jsx
@@ -47,7 +47,7 @@ const App = () => {
     
     try {
       const data = await axios.get(
-        "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=369&date="+ "31-05-2021"
+        "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByDistrict?district_id=369&date="+ current_date
       );
       setVaccine(data.data.centers);
 
